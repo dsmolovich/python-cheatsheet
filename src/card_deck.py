dc0494a688d3c84eaa9db395dc565ed7f5933351
Dmitry Smolovich <dsmolovich@gmail.com>
2024-03-30 15:37:33 -0400

    updates card deck

--- src/card_deck.py
+++ src/card_deck.py
@@ -6,7 +6,7 @@ Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-    suits = 'Spades Diamonds Clubs Hearts'.split(' ')
+    suits = 'spades diamonds clubs hearts'.split(' ')
     
     def __init__(self):
         self._cards = [Card(rank, suit)
@@ -19,8 +19,18 @@ class FrenchDeck:
     def __getitem__(self, position):
         return self._cards[position]
 
-if __name__ == '__main__':
-    deck = FrenchDeck()
-    for i in range(0, 6):
-        card = choice(deck)
-        print(card)
+
+deck = FrenchDeck()
+for i in range(0, 6):
+    card = choice(deck)
+    print(card)
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+def spades_high(card: Card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+print("Sorted: ===============")
+for card in sorted(deck, key=spades_high):
+    print(card)
