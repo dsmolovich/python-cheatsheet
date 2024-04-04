7724b8c29707f588d2e95f4b6ea41f8c00cf6b16
Dmitry Smolovich <dsmolovich@gmail.com>
2024-04-03 20:29:28 -0400

    adds slices

---
+++ src/fluent_python/chapter_2/slices.py
@@ -0,0 +1,27 @@
+
+address_bbok = """
+0...........12..............28..................48................
+John        Doe             +1(416)567.3456     john.doe@gmail.com
+Steven      Pearce          +1(905)212.5678     spearce@yahoo.com
+Frank       Frakhofost      +1(219)345.4412     franky@gmail.com
+"""
+
+FIRST_NAME = slice(0, 12)
+LAST_NAME = slice(12, 28)
+PHONE_NUMBER = slice(28, 48)
+EMAIL = slice(48, None)
+
+line_items = address_bbok.split("\n")[2:]
+
+def populate_people():
+    """
+    >>> populate_people()
+    John Doe john.doe@gmail.com
+    Steven Pearce spearce@yahoo.com
+    Frank Frakhofost franky@gmail.com
+    """
+    for line_item in line_items:
+        if line_item:
+            print(line_item[FIRST_NAME].strip(),
+                line_item[LAST_NAME].strip(),
+                line_item[EMAIL].strip())
