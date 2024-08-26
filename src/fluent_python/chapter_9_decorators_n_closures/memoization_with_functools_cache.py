160af9e16775dbc73264d682e6556dae20e30203
Dmitry Smolovich <dsmolovich@gmail.com>
2024-08-26 16:43:03 -0400

    Chapter 9: adds memoization with functools.cache

---
+++ src/fluent_python/chapter_9_decorators_n_closures/memoization_with_functools_cache.py
@@ -0,0 +1,25 @@
+import functools
+from implementing_decorator import clock
+
+@clock
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n-2) + fibonacci(n-1)
+
+@functools.cache
+@clock
+def fibonacci_with_cache(n):
+    if n < 2:
+        return n
+    return fibonacci_with_cache(n-2) + fibonacci_with_cache(n-1)
+
+
+if __name__ == '__main__':
+    num = 6
+    print("===========================================")
+    print(f"fibbonaci({num}):")
+    print(fibonacci(num))
+    print("===========================================")
+    print(f"fibonacci_with_cache({num}):")
+    print(fibonacci_with_cache(num))
