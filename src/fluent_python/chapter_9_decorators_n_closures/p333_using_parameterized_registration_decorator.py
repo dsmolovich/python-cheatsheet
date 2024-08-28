a2f9ceda4d54a5ae3eb12c27c269d07032ffd210
Dmitry Smolovich <dsmolovich@gmail.com>
2024-08-28 17:08:48 -0400

    Chapter 9: adds parameterized registration decorator

---
+++ src/fluent_python/chapter_9_decorators_n_closures/p333_using_parameterized_registration_decorator.py
@@ -0,0 +1,23 @@
+"""
+>>> from src.fluent_python.chapter_9_decorators_n_closures.p331_parameterized_registration_decorator import *  #doctest: +ELLIPSIS
+running register(active=False)->decorate(<function f1 at 0x...>)
+running register(active=True)->decorate(<function f2 at 0x...>)
+
+>>> registry  #doctest: +ELLIPSIS
+{<function f2 at 0x...>}
+
+>>> register()(f3)  #doctest: +ELLIPSIS
+running register(active=True)->decorate(<function f3 at 0x...>)
+<function f3 at 0x...>
+
+>>> registry  #doctest: +ELLIPSIS
+{<function f2 at 0x...>, <function f3 at 0x...>}
+
+>>> register(active = False)(f2)  #doctest: +ELLIPSIS
+running register(active=False)->decorate(<function f2 at 0x...>)
+<function f2 at 0x...>
+
+>>> registry  #doctest: +ELLIPSIS
+{<function f3 at 0x...>}
+"""
+
